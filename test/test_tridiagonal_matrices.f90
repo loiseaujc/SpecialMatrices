c2ffb87ded82d0a3361af5f543ba8de6e15275d5
Jean-Christophe <loiseau.jc@gmail.com>
2024-10-31 16:40:28 +0100

    Added Diagonal solver test.

--- test/test_tridiagonal_matrices.f90
+++ test/test_tridiagonal_matrices.f90
@@ -1,7 +1,8 @@
 module TestTridiag
     ! Fortran standard library.
-    use stdlib_kinds, only: dp
     use stdlib_math, only: is_close, all_close
+    use stdlib_linalg_constants, only: dp, ilp
+    use stdlib_linalg, only: solve
     ! Testdrive.
     use testdrive, only: new_unittest, unittest_type, error_type, check
     ! SpecialMatrices
@@ -23,16 +24,17 @@ contains
         type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
         testsuite = [ &
-                    new_unittest("Diagonal contructors", test_diagonal_constructors) &
+                    new_unittest("Diagonal contructors", test_diagonal_constructors),  &
+                    new_unittest("Diagonal linear solver", test_diagonal_solve)  &
                     ]
         return
     end subroutine collect_diagonal_testsuite
 
     subroutine test_diagonal_constructors(error)
         type(error_type), allocatable, intent(out) :: error
-        type(Diagonal), allocatable :: A
+        type(Diagonal) :: A
         real(dp) :: dv(n), d
-        integer :: i
+        integer(ilp) :: i
 
         ! Zero-initialization of Diagonal.
         A = Diagonal(n)
@@ -53,6 +55,44 @@ contains
 
         return
     end subroutine test_diagonal_constructors
+
+    subroutine test_diagonal_solve(error)
+        type(error_type), allocatable, intent(out) :: error
+        type(Diagonal) :: A
+        real(dp) :: dv(n)
+        
+        ! Initialize matrix.
+        call random_number(dv); A = Diagonal(dv)
+
+        ! Solve with a single right-hand side vector.
+        block
+        real(dp) :: x(n), x_stdlib(n), b(n)
+        ! Random rhs.
+        call random_number(b)
+        ! Solve with SpecialMatrices.
+        x = solve(A, b)
+        ! Solve with stdlib (dense).
+        x_stdlib = solve(dense(A), b)
+        ! Check error.
+        call check(error, all_close(x, x_stdlib), &
+        "Diagonal solve with a single rhs failed.")
+        if (allocated(error)) return
+        end block
+
+        ! Solve with multiple right-hand side vectors.
+        block
+        real(dp) :: x(n, n), x_stdlib(n, n), b(n, n)
+        ! Random rhs.
+        call random_number(b)
+        ! Solve with SpecialMatrices.
+        x = solve(A, b)
+        ! Solve with stdlib (dense).
+        x_stdlib = solve(dense(A), b)
+        ! Check error.
+        call check(error, all_close(x, x_stdlib), &
+        "Diagonal solve with multiple rhs failed.")
+        end block
+    end subroutine test_diagonal_solve
 
     !---------------------------------------
     !-----     BIDIAGONAL MATRICES     -----
