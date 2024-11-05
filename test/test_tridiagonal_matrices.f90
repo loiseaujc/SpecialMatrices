577fa7550ee827c8b1dae3666c51f191ec73248a
Jean-Christophe <loiseau.jc@gmail.com>
2024-11-05 11:18:23 +0100

    Added tests for Tridiagonal matrices (trace, det, scalar mult).

--- test/test_tridiagonal_matrices.f90
+++ test/test_tridiagonal_matrices.f90
@@ -24,6 +24,9 @@ contains
       type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
       testsuite = [ &
+                  new_unittest("Tridiagonal scalar multiplication", test_scalar_multiplication), &
+                  new_unittest("Tridiagonal trace", test_trace), &
+                  new_unittest("Tridiagonal determinant", test_det), &
                   new_unittest("Tridiagonal matmul", test_matmul), &
                   new_unittest("Tridiagonal linear solver", test_solve) &
                   ]
@@ -105,5 +108,64 @@ contains
 
       return
    end subroutine test_solve
+
+   subroutine test_scalar_multiplication(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(Tridiagonal) :: A, B
+      real(dp), allocatable :: dl(:), dv(:), du(:)
+      real(dp) :: alpha
+
+      ! Initialize matrix.
+      allocate (dl(n - 1), dv(n), du(n - 1))
+      call random_number(dl); call random_number(dv); call random_number(du)
+      A = TriDiagonal(dl, dv, du)
+      call random_number(alpha)
+
+      ! Scalar-matrix multiplication.
+      B = alpha*A
+      ! Check error.
+      call check(error, all_close(alpha*dense(A), dense(B)), &
+                 "alpha*Tridiagonal failed.")
+      if (allocated(error)) return
+
+      ! Matrix-scalar multipliation.
+      B = A*alpha
+      ! Check error.
+      call check(error, all_close(alpha*dense(A), dense(B)), &
+                 "Tridiagonal*alpha failed.")
+      return
+   end subroutine test_scalar_multiplication
+
+   subroutine test_trace(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(Tridiagonal) :: A
+      real(dp), allocatable :: dl(:), dv(:), du(:)
+
+      ! Initialize matrix.
+      allocate (dl(n-1), dv(n), du(n-1))
+      call random_number(dl); call random_number(dv); call random_number(du)
+      A = Tridiagonal(dl, dv, du)
+
+      ! Compare against stdlib_linalg implementation.
+      call check(error, is_close(trace(A), trace(dense(A))), &
+                 "Tridiagonal trace failed.")
+      return
+   end subroutine test_trace
+
+   subroutine test_det(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(Tridiagonal) :: A
+      real(dp), allocatable :: dl(:), dv(:), du(:)
+
+      ! Initialize matrix.
+      allocate (dl(n-1), dv(n), du(n-1))
+      call random_number(dl); call random_number(dv); call random_number(du)
+      A = Tridiagonal(dl, dv, du)
+
+      ! Compare against stdlib_linalg implementation.
+      call check(error, is_close(det(A), det(dense(A))), &
+                 "SymTridiagonal det failed.")
+      return
+   end subroutine test_det
 
 end module
