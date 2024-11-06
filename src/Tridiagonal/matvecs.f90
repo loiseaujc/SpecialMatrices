ba17b34dd41065b680da654c07305d77527eaeb2
Jean-Christophe <loiseau.jc@gmail.com>
2024-11-06 15:35:57 +0100

    Tridiagonal matmul now uses lagtm.

--- src/Tridiagonal/matvecs.f90
+++ src/Tridiagonal/matvecs.f90
@@ -1,23 +1,36 @@
 submodule(specialmatrices_tridiagonal) tridiagonal_matvecs
+   use stdlib_linalg_lapack, only: lagtm
    implicit none(type, external)
 contains
    module procedure spmv
-   integer(ilp) :: i, n
-   n = A%n; allocate (y, mold=x)
-   y(1) = A%dv(1)*x(1) + A%du(1)*x(2)
-   do concurrent(i=2:n - 1)
-      y(i) = A%dl(i - 1)*x(i - 1) + A%dv(i)*x(i) + A%du(i)*x(i + 1)
-   end do
-   y(n) = A%dv(n)*x(n) + A%dl(n - 1)*x(n - 1)
+   ! Local variables.
+   character :: trans
+   integer(ilp) :: n, nrhs, ldx, ldy
+   real(dp), parameter :: alpha = 1.0_dp, beta = 0.0_dp
+   real(dp), allocatable :: dl(:), dv(:), du(:)
+   real(dp), pointer :: xmat(:, :), ymat(:, :)
+
+   ! Setup variables.
+   n = A%n; nrhs = 1; ldx = n; ldy = n; trans = "N"
+   dl = A%dl; dv = A%dv; du = A%du
+   y = x; xmat(1:n, 1:nrhs) => x; ymat(1:n, 1:nrhs) => y
+   
+   ! Matrix-vector product.
+   call lagtm(trans, n, nrhs, alpha, dl, dv, du, xmat, ldx, beta, ymat, ldy)
    end procedure
 
    module procedure spmvs
-   integer(ilp) :: i, j, n, nvecs
-   n = A%n; nvecs = size(x, 2); allocate (y, mold=x)
-   y(1, :) = A%dv(1)*x(1, :) + A%du(1)*x(2, :)
-   do concurrent(i=2:n - 1, j=1:nvecs)
-      y(i, j) = A%dl(i - 1)*x(i - 1, j) + A%dv(i)*x(i, j) + A%du(i)*x(i + 1, j)
-   end do
-   y(n, :) = A%dv(n)*x(n, :) + A%dl(n - 1)*x(n - 1, :)
+   ! Local variables.
+   character :: trans
+   integer(ilp) :: n, nrhs, ldx, ldy
+   real(dp), parameter :: alpha = 1.0_dp, beta = 0.0_dp
+   real(dp), allocatable :: dl(:), dv(:), du(:)
+
+   ! Setup variables.
+   n = A%n; nrhs = size(x, 2); ldx = n; ldy = n; trans = "N"
+   dl = A%dl; dv = A%dv; du = A%du; y = x
+
+   ! Matrix-vector product.
+   call lagtm(trans, n, nrhs, alpha, dl, dv, du, x, ldx, beta, y, ldy)
    end procedure
 end submodule
